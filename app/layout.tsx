d85bc7d2cae761cb10d487d814a392be0b7b2f80
Gohar Ali <ghulammujtaba074@gmail.com>
2024-08-18 10:05:55 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+//import { Inter } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/navbar";
+
+//const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Snowcap Adventures",
+  description: "Travel the world with Snowcap Adventures",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
