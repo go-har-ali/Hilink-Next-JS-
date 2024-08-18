cfe4d9e7ade7703d915a13bd4eb18cd8c1edb916
Gohar Ali <ghulammujtaba074@gmail.com>
2024-08-18 10:17:13 +0500

    second commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -59,7 +59,7 @@ const Hero = () => {
             />
           </div>
         </div> 
-         <div className='relative flex flex-1 items-start mr-28'>
+         <div className='relative flex flex-1 items-start'>
           <div className='relative z-20 flex w-[268px] flex-col gap-8 
           rounded-3xl bg-green-90 px-7 py-8'>
             
